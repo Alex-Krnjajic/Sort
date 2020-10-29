d4539fc04557bd397c61ecf1bba93f6c777c99c3
Alex <alexk753@gmail.com>
2020-10-29 11:46:15 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,119 @@
+package sample;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Controller {
+
+
+
+    private final int min = 10;
+    private final int max = 300;
+
+    double minHeight;
+    int minIndex = 0;
+    int counter = 0;
+    double temp = 0;
+
+    public static ArrayList<Rectangle> RectList = new ArrayList<Rectangle>();
+    double[] RectHeight;
+
+
+    public void RectUpdater() {
+
+
+        Rectangle temp = RectList.get(1);
+        System.out.println(temp.getHeight());
+
+
+    }
+
+    @FXML
+    public void randomHeight() {
+
+        RectHeight = new double[RectList.size()];
+        int i = 0;
+
+
+        for (Rectangle rect : RectList
+        ) {
+            double randHeight = Math.random() * (max - min + 1) + min;
+            RectHeight[i] = randHeight;
+            rect.setHeight(randHeight);
+            i++;
+
+
+        }
+
+        minHeight = RectHeight[0];
+    }
+
+    @FXML
+    public void sort() throws InterruptedException {
+        Timeline timeline = new Timeline();
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(100), e -> {
+            try {
+                sort2();
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+        }));
+        timeline.setOnFinished(e ->
+                System.out.println(Arrays.toString(RectHeight))
+                );
+        timeline.setCycleCount(RectList.size()-1);
+        minHeight = RectHeight[0];
+        counter = 0;
+        minIndex = 0;
+        temp = 0;
+
+        timeline.play();
+
+        System.out.println(Arrays.toString(RectHeight));
+
+
+    }
+    public void sort2() throws InterruptedException {
+        System.out.println("start sort");
+
+        System.out.println(minHeight);
+
+
+
+        System.out.println("find min");
+        for (int i = counter; i < (RectList.size()); i++) {
+            //System.out.println("i= " + i);
+            if (RectHeight[i] < minHeight) {
+                minHeight = RectHeight[i];
+               // System.out.println("minHeight= " + minHeight);
+               // System.out.println("minIndex= " + i);
+                minIndex = i;
+            }
+        }
+
+        updateHeight();
+        //Thread.sleep(500);
+
+    }
+
+    public void updateHeight() {
+        temp = RectHeight[counter];
+        RectHeight[counter] = RectHeight[minIndex];
+        RectList.get(counter).setHeight(RectHeight[counter]);
+
+        RectHeight[minIndex] = temp;
+        RectList.get(minIndex).setHeight(temp);
+
+
+        counter++;
+        minHeight = RectHeight[counter];
+        minIndex = counter;
+
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,87 @@
+package sample;
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Pane root = new Pane();
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Sort");
+
+        for(int i =0;i<180;i++){
+            Rectangle rect = new Rectangle();
+            rect.rotateProperty().setValue(180);
+            rect.setX((10*i));
+            rect.setY(150);
+            rect.setHeight(10);
+            rect.setWidth(5);
+            root.getChildren().add(rect);
+            Controller.RectList.add(rect);
+        }
+        Controller con = new Controller();
+
+        Button btn1 = new Button("random");
+        btn1.setLayoutX(10);
+        btn1.setLayoutY(10);
+        DropShadow shadow = new DropShadow();
+        btn1.setEffect(shadow);
+
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                // TODO Auto-generated method stub
+                con.randomHeight();
+
+            }
+        });
+        Button btn2 = new Button("sort");
+        btn2.setLayoutX(100);
+        btn2.setLayoutY(10);
+        DropShadow shadow2 = new DropShadow();
+        btn2.setEffect(shadow);
+
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                // TODO Auto-generated method stub
+                try {
+                    con.sort();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        root.getChildren().add(btn1);
+        root.getChildren().add(btn2);
+        Scene scene = new Scene(root, 1800, 540);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
